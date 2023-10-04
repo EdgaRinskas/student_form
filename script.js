18150711178623f676d26b9773e656111833749d
Edgaras Austinskas <eaustinskas@gmail.com>
2023-10-04 10:35:55 +0300

    1

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact-form");
+    const studentsList = document.getElementById("students-list");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const surname = document.getElementById("surname").value;
+        const age = document.getElementById("age").value;
+        const telephone = document.getElementById("telephone").value;
+        const email = document.getElementById("email").value;
+        const range = document.getElementById("range").value;
+        const group = document.querySelector('input[name="group"]:checked').value;
+        const languages = [...document.querySelectorAll('input[name="languages"]:checked')].map(input => input.value).join(", ");
+
+        // Create a new student item
+        const studentItem = document.createElement("div");
+        studentItem.classList.add("student-item");
+
+        // Populate student data
+        studentItem.innerHTML = `
+            <strong>Name:</strong> ${name}<br>
+            <strong>Surname:</strong> ${surname}<br>
+            <strong>Age:</strong> ${age}<br>
+            <strong>Telephone Number:</strong> ${telephone}<br>
+            <strong>Email:</strong> ${email}<br>
+            <strong>Range:</strong> ${range}<br>
+            <strong>Group:</strong> ${group}<br>
+            <strong>Languages:</strong> ${languages}
+        `;
+
+        // Add the student item to the top of the list
+        studentsList.insertBefore(studentItem, studentsList.firstChild);
+
+        // Display the data in the console
+        console.log("Name:", name);
+        console.log("Surname:", surname);
+        console.log("Age:", age);
+        console.log("Telephone Number:", telephone);
+        console.log("Email:", email);
+        console.log("Range:", range);
+        console.log("Group:", group);
+        console.log("Languages:", languages);
+
+        // Reset the form
+        form.reset();
+    });
+});
+
